594891fe90be402a679f28e35bbeb9045b4dd1fa
Máté Illés <mate.illes@centricdigital.com>
2018-01-09 13:04:09 +0100

    clear code

--- src/app/d3/index.ts
+++ src/app/d3/index.ts
@@ -2,7 +2,6 @@ export * from './models/node';
 export * from './models/link';
 export * from './d3.service';
 export * from './models';
-export * from './directives';
 
 // To be implemented in the next gist
 export * from './models/force-directed-graph';
