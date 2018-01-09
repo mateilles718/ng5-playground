df029f6e9c6cc08c416cc4f26f1c05b7b511c78a
Máté Illés <mate.illes@centricdigital.com>
2018-01-09 12:39:01 +0100

    add d3.js chart

--- src/app/app-home/app-home.component.ts
+++ src/app/app-home/app-home.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {DataService} from '../data.service';
+import {WeatherService} from '../weather.service';
+import {Chart} from 'chart.js';
+import {Link, Node} from '../d3/models';
+import APP_CONFIG from '../app.config';
 
 @Component({
   selector: 'app-app-home',
@@ -11,8 +15,11 @@ export class AppHomeComponent implements OnInit {
   itemCount: number;
   goalText: string;
   goals = [];
+  chart = [];
+  nodes: Node[] = [];
+  links: Link[] = [];
 
-  constructor(private _data: DataService) {
+  constructor(private _data: DataService, private _weather: WeatherService) {
   }
 
   ngOnInit() {
@@ -21,6 +28,80 @@ export class AppHomeComponent implements OnInit {
     });
     this.itemCount = this.goals.length;
     this._data.changeGoal(this.goals);
+
+    this.drawChartJs();
+    this.drawD3Js();
+  }
+
+  drawD3Js() {
+    const N = APP_CONFIG.N,
+      getIndex = number => number - 1;
+
+    /** constructing the nodes array */
+    for (let i = 1; i <= N; i++) {
+      this.nodes.push(new Node(i));
+    }
+
+    for (let i = 1; i <= N; i++) {
+      for (let m = 2; i * m <= N; m++) {
+        /** increasing connections toll on connecting nodes */
+        this.nodes[getIndex(i)].linkCount++;
+        this.nodes[getIndex(i * m)].linkCount++;
+
+        /** connecting the nodes before starting the simulation */
+        this.links.push(new Link(i, i * m));
+      }
+    }
+  }
+
+
+  drawChartJs() {
+    this._weather.dailyForecast()
+      .subscribe((res) => {
+        console.log(res);
+        const temp_max = res['list'].map(res => res.main.temp_max);
+        const temp_min = res['list'].map(res => res.main.temp_min);
+        const alldates = res['list'].map(res => res.dt);
+
+        const weatherDates = [];
+        alldates.forEach((res) => {
+          let jsdate = new Date(res * 1000);
+          weatherDates.push(jsdate.toLocaleTimeString('en', {year: 'numeric', month: 'short', day: 'numeric'}))
+        });
+
+        this.chart = new Chart('canvas', {
+          type: 'line',
+          data: {
+            labels: weatherDates,
+            datasets: [
+              {
+                data: temp_max,
+                borderColor: "#3cba9f",
+                fill: false
+              },
+              {
+                data: temp_min,
+                borderColor: "#ffcc00",
+                fill: false
+              },
+            ]
+          },
+          options: {
+            legend: {
+              display: false
+            },
+            scales: {
+              xAxes: [{
+                display: true
+              }],
+              yAxes: [{
+                display: true
+              }],
+              events: ['onHover']
+            }
+          }
+        });
+      });
   }
 
   addItem() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { AppHomeComponent } from './app-home/app-home.component';
-import { FormsModule } from '@angular/forms';
-import { AppAboutComponent } from './app-about/app-about.component';
-import { DataService } from './data.service';
-
+import {AppComponent} from './app.component';
+import {AppHomeComponent} from './app-home/app-home.component';
+import {FormsModule} from '@angular/forms';
+import {AppAboutComponent} from './app-about/app-about.component';
+import {DataService} from './data.service';
+import {WeatherService} from './weather.service';
+import {HttpClientModule} from "@angular/common/http";
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHomeComponent,
-    AppAboutComponent
+    AppAboutComponent,
+    BarChartComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [DataService],
+  providers: [DataService, WeatherService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
